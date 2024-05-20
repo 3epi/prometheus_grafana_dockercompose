d8fbac5bc8dd61a5e97fa28e092386f01f0a9bb1
sepehr <mohammadsepehr.bazargan@gmail.com>
2024-05-20 23:39:51 +0330

    consumer bootstrap server change

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -2,7 +2,7 @@ from kafka import KafkaConsumer
 
 consumer = KafkaConsumer('monthly-report',
                          group_id='my-group',
-                         bootstrap_servers=['localhost:9092'])
+                         bootstrap_servers=['kafka:9092'])
 for message in consumer:
     print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
                                           message.offset, message.key,
