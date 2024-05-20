8b11df4608c47838491ccb819bc72a6d611821d6
sepehr <mohammadsepehr.bazargan@gmail.com>
2024-05-20 23:20:57 +0330

    Remove Dockerfile, consumer.py, and producer.py from main directory

--- consumer.py
+++
@@ -1,9 +0,0 @@
-from kafka import KafkaConsumer
-
-consumer = KafkaConsumer('monthly-report',
-                         group_id='my-group',
-                         bootstrap_servers=['localhost:9092'])
-for message in consumer:
-    print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
-                                          message.offset, message.key,
-                                          message.value))
