490bd378799e142e93449c9ebaaf79af036169c8
sepehr <mohammadsepehr.bazargan@gmail.com>
2024-05-21 23:39:31 +0330

    writing data into mongoDB database

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -1,7 +1,8 @@
 from kafka import KafkaConsumer
 import time
+import pymongo
 
-def consume_messages(bootstrap_servers, topic, group_id='your_group'):
+def consume_messages(bootstrap_servers, topic, mycol , group_id='your_group'):
     while True:
         try:
             consumer = KafkaConsumer(topic,
@@ -12,6 +13,14 @@ def consume_messages(bootstrap_servers, topic, group_id='your_group'):
                 print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
                                           message.offset, message.key,
                                           message.value))
+                datadict = {
+                    "topic" : message.topic,
+                    "partition" : message.partition,
+                    "offset" : message.offset,
+                    "key" : message.key ,
+                    "value" : message.value
+                }
+                mycol.insert_one(datadict)
         except Exception as e:
             print(f"Error connecting to Kafka brokers: {e}")
             print("Retrying in 5 seconds...")
@@ -20,4 +29,7 @@ def consume_messages(bootstrap_servers, topic, group_id='your_group'):
 bootstrap_servers = 'kafka:9092'
 topic = 'mytopic'
 
-consume_messages(bootstrap_servers, topic)
+client = pymongo.MongoClient("mongo", 27017)
+mydb = client["mydatabase"]
+mycol = mydb["consumeddata"]
+consume_messages(bootstrap_servers, topic , mycol)
