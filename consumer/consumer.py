19db6a93e7cea0335d01f493310ba0660ade9771
sepehr <mohammadsepehr.bazargan@gmail.com>
2024-05-21 18:50:30 +0330

    consumer recieves incontinouesly

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -1,9 +1,23 @@
 from kafka import KafkaConsumer
+import time
 
-consumer = KafkaConsumer('monthly-report',
-                         group_id='my-group',
-                         bootstrap_servers=['kafka:29092'])
-for message in consumer:
-    print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
+def consume_messages(bootstrap_servers, topic, group_id='your_group'):
+    while True:
+        try:
+            consumer = KafkaConsumer(topic,
+                                     group_id=group_id,
+                                     bootstrap_servers=bootstrap_servers)
+            print(f"Connected to Kafka brokers at {bootstrap_servers} and consuming topic {topic}")
+            for message in consumer:
+                print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
                                           message.offset, message.key,
                                           message.value))
+        except Exception as e:
+            print(f"Error connecting to Kafka brokers: {e}")
+            print("Retrying in 5 seconds...")
+            time.sleep(5)
+
+bootstrap_servers = 'kafka:9092'
+topic = 'mytopic'
+
+consume_messages(bootstrap_servers, topic)
