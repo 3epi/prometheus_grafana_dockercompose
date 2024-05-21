176e0d1e7efdab437f0309d84320c26c3424d41f
sepehr <mohammadsepehr.bazargan@gmail.com>
2024-05-21 18:41:17 +0330

    producer now works fine and is connected to kafka container

--- producer/producer.py
+++ producer/producer.py
@@ -2,20 +2,26 @@ import time
 from kafka import KafkaProducer
 from kafka.errors import NoBrokersAvailable
 
-def create_kafka_producer(bootstrap_servers, max_retries=3, retry_interval=5):
+def create_kafka_producer(bootstrap_servers, max_retries=10, retry_interval=5):
     for attempt in range(1, max_retries + 1):
         try:
-            return KafkaProducer(bootstrap_servers=bootstrap_servers)
+            print(f"Attempt {attempt}: Trying to connect to Kafka brokers at {bootstrap_servers}...")
+            producer = KafkaProducer(bootstrap_servers=bootstrap_servers)
+            print("Successfully connected to Kafka brokers.")
+            return producer
         except NoBrokersAvailable:
             print(f"Attempt {attempt}: No brokers available. Retrying in {retry_interval} seconds...")
             time.sleep(retry_interval)
     raise RuntimeError("Failed to connect to Kafka brokers after multiple attempts.")
 
-bootstrap_servers = 'localhost:29092'
+bootstrap_servers = 'kafka:9092'
 
 # Create Kafka producer with retry mechanism
 producer = create_kafka_producer(bootstrap_servers)
 
-producer.send('mytopic', b"test message")
-producer.flush()
+while (True):
+    producer.send('mytopic', b"test message")
+    print("message sent")
+    producer.flush()
+
 producer.close()
