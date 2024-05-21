3dcc0f7c780266303ca5c2a611eff3301a727407
sepehr <mohammadsepehr.bazargan@gmail.com>
2024-05-21 23:02:18 +0330

    producer now generates random data

--- producer/producer.py
+++ producer/producer.py
@@ -1,6 +1,14 @@
 import time
 from kafka import KafkaProducer
 from kafka.errors import NoBrokersAvailable
+import random
+import string
+import time
+
+def generate_random_string(length=random.randint(10 , 1000)):
+    letters = string.ascii_letters + string.digits
+    return ''.join(random.choice(letters) for _ in range(length))
+
 
 def create_kafka_producer(bootstrap_servers, max_retries=10, retry_interval=5):
     for attempt in range(1, max_retries + 1):
@@ -20,8 +28,10 @@ bootstrap_servers = 'kafka:9092'
 producer = create_kafka_producer(bootstrap_servers)
 
 while (True):
-    producer.send('mytopic', b"test message")
-    print("message sent")
+    message = generate_random_string()
+    producer.send('mytopic', message)
+    print("{message} sent")
+    time.sleep(5)
     producer.flush()
 
 producer.close()
